46d447b9daf6271c05a2b8d3492b9c951fdd8f6e
João Gabriel <joao.royosilva@gmail.com>
2014-04-08 22:34:10 -0400

    Adição de barra de rolagem e listagem de chamados

--- sistema/view/call/list.php
+++ sistema/view/call/list.php
@@ -9,20 +9,48 @@
 
 <!-- Lista de chamados (um chamado por Div) -->
 <div class="alert alert-info">
-    <div class="row-fluid">
-        <div class="span8">
-            <b>Número:</b> 1
-        </div>
-        <div class="span4">
-            <b>Abertura:</b> 24/04/2014 - 15:00
-        </div>
+    <div>
+        <b>Número:</b> 1
     </div>
-    <div class="row-fluid">
-        <div class="span8">
-            <b>Cliente:</b> Lojas Center Car
-        </div>
-        <div class="span4">
-            <b>Problema:</b> Computador não conecta no servidor.
-        </div>
-    </div>  
+    <div>
+        <b>Abertura:</b> 24/04/2014 - 15:00
+    </div>
+    <div>
+        <b>Cliente:</b> Lojas Center Car
+    </div>
+    <div>
+        <b>Problema:</b> Computador não conecta no servidor.
+    </div>
+    <a class="btn btn-primary" href="#" ><i class="glyphicon glyphicon-file"></i> Atender</a>
+</div>
+<div class="alert alert-danger">
+    <div>
+        <b>Número:</b> 2
+    </div>
+    <div>
+        <b>Abertura:</b> 03/04/2014 - 15:00
+    </div>
+    <div>
+        <b>Cliente:</b> Advocacia Toledo
+    </div>
+    <div>
+        <b>Problema:</b> Sistema não abre.
+    </div>
+    <a class="btn btn-primary" href="#" ><i class="glyphicon glyphicon-file"></i>  Atender</a>
+</div>
+<div class="alert alert-success">
+    <div>
+        <b>Número:</b> 3
+    </div>
+    <div>
+        <b>Abertura:</b> 04/04/2014 - 15:00
+    </div>
+    <div>
+        <b>Cliente:</b> Farmácia Santa Elena
+    </div>
+    <div>
+        <b>Problema:</b> Impressora sem comunicação.
+    </div>
+    <a class="btn btn-primary disabled" href="#" ><i class="glyphicon glyphicon-ok"></i> Atendido</a>
+</div>
 </div>
--- sistema/view/index/index.php
+++ sistema/view/index/index.php
@@ -7,7 +7,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="">
     <meta name="author" content="">
-    <link href="../bootstrap/css/bootstrap.css" rel="stylesheet">
+    <link href="../bootstrap/css/bootstrap.css" rel="stylesheet">  
     <style> body { padding-top: 60px; } </style>
   </head>
   <body>
